f51a1177c09932e78f2ae9943208da10d6efb0ea
PanosAndrik <katholoucrew@gmail.com>
2024-11-10 12:09:35 +0200

    api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,1 +1,7 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4040';
+const cleanUrl = (url) => url.replace(/([^:]\/)\/+/g, "$1"); 
+
+export const API_URL = cleanUrl(import.meta.env.VITE_API_URL || 'http://localhost:4040');
+
+
+export const apiCall = (endpoint) => {
+  return `${API_URL}/${endpoint.replace(/^\/+/, '')}`; /
